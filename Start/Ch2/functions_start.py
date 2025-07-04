223b62a2607fca8d7d1b1aa0e5b37f75bcc4709f
chinnapiyaa <chinna.piyaa@gmail.com>
2025-07-04 13:37:41 +0000

    changes

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,17 +3,43 @@
 
 
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
-
+#def greet_fun():
+ #   print("hello world!")
+  #  name = input("What is your name? ")
+   # print("Nice to meet you,", name)
+    #lastname = input("what is your last name?")
+    #print("thank you for sharing", name+" "+lastname)
+#greet_fun()
 # function that takes parameters
-
+#def greet_fun(greeting):
+ #   print("hello world!")
+   # name = input("What is your name? ")
+    #print(greeting,name)
+#greet_fun("how are you")
+#greet_fun("whats up")
 
 # function that returns a value
-
+#def double_it(X):
+ # return X*2
+#result = double_it(5)
+#print("result is",result)
 
 # function with default value for an parameter
-
+#def greet_fun(greeting, name=None):
+ #  print("hello world!")
+  # if name == None:
+   #    name = input("what is your name")
+   #print(greeting,name)
+   # print("Nice to meet you,", name)
+    #lastname = input("what is your last name?")
+    #print("thank you for sharing", name+" "+lastname)
+#greet_fun(name="apoo", greeting="hey")
+#greet_fun("how are you")
 
 # function with variable number of parameters
+def multi_Add(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
+    print(multi_Add(1,2,3,4,5))
--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -2,18 +2,36 @@
 # Example file for working with loops
 
 
-x = 0
+#x = 0
 
 # define a while loop
 
-
+#while x < 5:
+ # print(x)
+  #x = x+1
+ # answer = input ("should i stop?")
+  #while answer !="yes":
+   # print(answer)
+    #answer = input("should i stop?")
 # define a for loop
-
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+#for daynames in days:
+ # print(daynames)
 
 # use a for loop over a collection
 
 
 # use the break and continue statements
 
+#for d in days:
+ # if( d == "Thu"):
+  #  break # to skip the rest of the loop
+  #print(d)
+#for d in days:
+ # if( d == "Thu"):
+  #  continue # to skip just the "thu" and continue with the rest of the loop#
+  #print(d)
 
 # using the enumerate() function to get an index and an item
+for i , d in enumerate(days):
+    print(i,d)
